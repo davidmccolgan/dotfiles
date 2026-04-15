606685f7d9fc241815a1d1e136690b7d877c504d
dav <dav@Mac.pfsence.flatcapsoftware.com>
2026-04-14 23:12:05 -0400

    initial dotfiles

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+}
---
+++ zsh/.zshrc
@@ -0,0 +1,2 @@
+export PATH="$HOME/.local/bin:$PATH"
+export ANTHROPIC_API_KEY="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"
